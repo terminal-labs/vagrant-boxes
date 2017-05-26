540da651f7eacfb6d8bcc92e3b0bb8fe9a3553df
user <michael@terminallabs.com>
2017-05-26 17:01:58 -0500

    updated packer json files

--- packer/build_boxes.sh
+++ packer/build_boxes.sh
@@ -1,1 +1,8 @@
+./packer build debian/debian8_2tb.json
 ./packer build debian/debian8_10gb.json
+
+./packer build debian/debian8_20gb.json
+./packer build debian/debian8_30gb.json
+./packer build debian/debian8_40gb.json
+./packer build debian/debian8_60gb.json
+./packer build debian/debian8_80gb.json
