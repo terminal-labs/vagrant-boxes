f1bf878a81bd10857f2e19b82ffb89c0697a7765
user <michael@terminallabs.com>
2017-06-06 18:47:28 -0500

    more work on batch processing

--- packer/build_boxes.sh
+++ packer/build_boxes.sh
@@ -1,2 +1,11 @@
 ./packer build config_files/debian/debian8_2tb.json
-./packer build config_files/ubuntu/ubuntu16_2tb.json
+
+./packer build config_files/debian/debian8_20gb.json
+./packer build config_files/debian/debian8_30gb.json
+./packer build config_files/debian/debian8_40gb.json
+./packer build config_files/debian/debian8_60gb.json
+./packer build config_files/debian/debian8_80gb.json
+
+
+
+#./packer build config_files/ubuntu/ubuntu16_2tb.json
