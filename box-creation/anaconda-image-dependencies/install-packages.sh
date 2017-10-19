38ba0d968faac77362a336546002f992ede43aef
Michael Verhulst <michael@terminallabs.com>
2017-10-19 13:04:34 -0500

    now building anaconda boxes for tl-debian-jessie-64bit-2tb-anaconda and tl-ubuntu-1604-64bit-2tb-anaconda

--- box-creation/anaconda-image-dependencies/install-packages.sh
+++ box-creation/anaconda-image-dependencies/install-packages.sh
@@ -1,6 +1,4 @@
-cd /vagrant/.full-image-dependencies
+cd /vagrant/.anaconda-image-dependencies
 bash system-packages/aptget-packages.sh
 bash system-packages/apt-packages.sh
 bash system-packages/misc-packages.sh
-bash package-managers/pip-packages.sh
-bash package-managers/node-packages.sh
