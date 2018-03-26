389cd5bc02923dab5b0aac622eb58ab4af50e638
Joseph Nix <nixjdm@terminallabs.com>
2018-03-26 17:07:20 -0500

    [setup-refactor] Add build-json cmd.

--- vagrant_boxes/cli.py
+++ vagrant_boxes/cli.py
@@ -9,9 +9,9 @@ def download_packer():
     # TODO: replace the following with Python
     # TODO: Download the most recent stable Packer, not pinned version (unless stable is broken)
     cmd = '''cd vagrant_boxes/packer
-wget https://releases.hashicorp.com/packer/1.0.0/packer_1.0.0_linux_amd64.zip
-unzip -o packer_1.0.0_linux_amd64.zip
-rm packer_1.0.0_linux_amd64.zip'''
+    wget https://releases.hashicorp.com/packer/1.0.0/packer_1.0.0_linux_amd64.zip
+    unzip -o packer_1.0.0_linux_amd64.zip
+    rm packer_1.0.0_linux_amd64.zip'''
     # TODO: replace with Joe's shell invoker
     os.system(cmd)
 
@@ -28,15 +28,16 @@ def cli(ctx):
     '''
 
 ### Commands
-@cli.command('setup')
-@click.option('-f', '--force', is_flag=True,
-              help='Agree to remove all vagrant boxes')
-@click.pass_context
-def setup_cmd(ctx, force):
-    '''Download Packer and remove Vagrant boxes.
+@cli.command('build-json')
+def build_json_cmd():
+    '''build intermediate json files
     '''
-    download_packer()
-    ctx.invoke(remove_boxes_cmd, force=force)
+    cmd = '''cd vagrant_boxes
+    mkdir -p bash-scripts
+    yasha yasha-templates/bash-templates/build-json-files.sh.template -o bash-scripts/build-json-files.sh
+    bash bash-scripts/build-json-files.sh'''
+    os.system(cmd)
+
 
 @cli.command('remove-boxes')
 @click.option('-f', '--force', is_flag=True,
@@ -49,5 +50,15 @@ def remove_boxes_cmd(force):
         os.system(cmd)
     elif click.confirm('Do you want to remove all vagrant boxes?'):
         os.system(cmd)
+
+@cli.command('setup')
+@click.option('-f', '--force', is_flag=True,
+              help='Agree to remove all vagrant boxes')
+@click.pass_context
+def setup_cmd(ctx, force):
+    '''Download Packer and remove Vagrant boxes.
+    '''
+    download_packer()
+    ctx.invoke(remove_boxes_cmd, force=force)
 
 main = cli
